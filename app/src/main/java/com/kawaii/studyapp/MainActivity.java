ff66bebdef195dc42b2b682ee11287ed6f95be37
Molokov-Klim <ultrakawaii9654449192@gmail.com>
2022-01-24 15:49:30 +0300

    finish list

--- app/src/main/java/com/kawaii/studyapp/MainActivity.java
+++ app/src/main/java/com/kawaii/studyapp/MainActivity.java
@@ -3,13 +3,26 @@ package com.kawaii.studyapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView plList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        plList = findViewById(R.id.tv_pl_names);
+
+        String[] names = {"Java", "C++", "JavaScript", "Delphi", "PHP", "GNU bc", "Euphoria", "C#", "ColdFusion", "D", "Dart", "Object Pascal", "Dylan", "Eiffel", "Groovy", "Haxe", "Io", "MC#"};
+
+        plList.setText("");
+        for(String name : names) {
+            plList.append(name + "\n");
+        }
+
+
     }
 }
 
